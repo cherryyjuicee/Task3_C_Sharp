0347964c132b65653f56da7d9d9545fc3c1135f1
cherryyjuicee <67759831+cherryyjuicee@users.noreply.github.com>
2021-11-06 19:24:16 +0300

    Add files via upload

---
+++ task3/task3/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace task3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(353, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(353, 137);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(353, 166);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "*";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(353, 195);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "/";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(165, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(431, 34);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Создать объект класса Простая дробь, используя класс Число.\r\nМетоды: вывод, сложе" +
+    "ние, вычитание, умножение, деление.\r\n";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(99, 128);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(173, 32);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(99, 166);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(173, 32);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(515, 128);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(173, 32);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(515, 166);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(173, 32);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(303, 299);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(173, 32);
+            this.textBox5.TabIndex = 10;
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Таск 3. Задача 12";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ task3/task3/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "")
+            {
+                MessageBox.Show("Введите числа!");
+            }
+
+            else
+            {
+                Decimal obj = new Decimal(new Number(int.Parse(textBox1.Text)), new Number(int.Parse(textBox2.Text)));
+                Decimal obj2 = new Decimal(new Number(int.Parse(textBox3.Text)), new Number(int.Parse(textBox4.Text)));
+
+                textBox5.Text = obj.Plus(obj2).ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "")
+            {
+                MessageBox.Show("Введите числа!");
+            }
+            else
+            {
+                Decimal obj = new Decimal(new Number(int.Parse(textBox1.Text)), new Number(int.Parse(textBox2.Text)));
+                Decimal obj2 = new Decimal(new Number(int.Parse(textBox3.Text)), new Number(int.Parse(textBox4.Text)));
+
+                textBox5.Text = obj.Minus(obj2).ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "")
+            {
+                MessageBox.Show("Введите числа!");
+            }
+            else
+            {
+                Decimal obj = new Decimal(new Number(int.Parse(textBox1.Text)), new Number(int.Parse(textBox2.Text)));
+                Decimal obj2 = new Decimal(new Number(int.Parse(textBox3.Text)), new Number(int.Parse(textBox4.Text)));
+
+                textBox5.Text = obj.Multiply(obj2).ToString();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "")
+            {
+                MessageBox.Show("Введите числа!");
+            }
+            else
+            { 
+            Decimal obj = new Decimal(new Number(int.Parse(textBox1.Text)), new Number(int.Parse(textBox2.Text)));
+            Decimal obj2 = new Decimal(new Number(int.Parse(textBox3.Text)), new Number(int.Parse(textBox4.Text)));
+
+            textBox5.Text = obj.Divide(obj2).ToString();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+        }
+    }
+
+    class Number
+    {
+        public int value;
+
+        public Number(int value)
+        {
+            this.value = value;
+        }
+    }
+
+    class Decimal
+    {
+        public Number num1;
+        public Number num2;
+
+        public Decimal(Number num1, Number num2)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+        }
+
+        public override string ToString()
+        {
+            int m = 1;
+
+            if (this.num1.value < 0 || this.num2.value < 0)
+            {
+                if (this.num1.value < 0 && this.num2.value > 0)
+                { 
+                    this.num1.value = this.num1.value * -1; 
+                    m = 0;
+                }
+                else if (this.num2.value < 0 && this.num1.value > 0)
+                {
+                    this.num2.value = this.num2.value * -1;
+                    m = 0;
+                }
+                else{
+                    this.num1.value = this.num1.value * -1;
+                    this.num2.value = this.num2.value * -1;
+                }
+            }
+
+
+            int o = 0;
+
+            if (this.num1.value > this.num2.value)
+            {
+                int i = this.num2.value;
+
+                while (i > 0)
+                {
+                    if (this.num1.value % i == 0 && this.num2.value % i == 0)
+                    {
+                        this.num1 = new Number(this.num1.value / i);
+                        this.num2 = new Number(this.num2.value / i);
+                        break;
+                    }
+                    else
+                    {
+                        i--;
+                    }
+                }
+
+                while (this.num1.value > this.num2.value)
+                {
+                    this.num1.value = this.num1.value - this.num2.value;
+                    o++;
+                }
+
+            }
+            
+            else
+            {
+                int i = this.num1.value;
+
+                while (i > 0)
+                {
+                    if (this.num1.value % i == 0 && this.num2.value % i == 0)
+                    {
+                        this.num1 = new Number(this.num1.value / i);
+                        this.num2 = new Number(this.num2.value / i);
+                        break;
+                    }
+                    else
+                    {
+                        i--;
+                    }
+                }
+            }
+            if (this.num1.value == 0)
+            {
+                return "0";
+            }
+            else if (this.num1.value == this.num2.value && o == 0 && m == 0)
+            {
+                return "-1";
+            }
+            else if (this.num1.value == this.num2.value && o == 0 && m != 0)
+            {
+                return "1";
+            }
+            else if (this.num1.value == this.num2.value && o != 0 && m != 0)
+            {
+                o = o + 1;
+                return o + "";
+            }
+            else if (this.num1.value == this.num2.value && o != 0 && m == 0)
+            {
+                m = (o + 1)*-1;
+                return m + "";
+            }
+            else if (this.num2.value == 1 && o != 0 && m != 0)
+            {
+                return o + "" + this.num1.value +"";
+            }
+            else if (this.num2.value == 1 && o != 0 && m == 0)
+            {
+                return "-" + o + "" + this.num1.value + "";
+            }
+            else if (this.num2.value == 1 && o == 0 && m != 0)
+            {
+                o = o + this.num1.value;
+                return o+"";
+            }
+            else if (this.num2.value == 1 && o == 0 && m == 0)
+            {
+                o = o + this.num1.value;
+                return "-" + o + "";
+            }
+            else if (o == 0 && m != 0)
+            {
+                return this.num1.value + "/" + this.num2.value ;
+            }
+            else if (o == 0 && m == 0)
+            {
+                return "-" + this.num1.value + "/" + this.num2.value;
+            }
+            else if (o != 0 && m != 0)
+            {
+                return o + " " + this.num1.value + "/" + this.num2.value;
+            }
+            else if (o != 0 && m == 0)
+            {
+                return "-" + o + " " + this.num1.value + "/" + this.num2.value;
+            }
+            else
+            {
+                return "Ошибка";
+            }
+        }
+
+        public Decimal Multiply(Decimal a)
+        {
+            Number d = new Number(this.num1.value * a.num1.value);
+            Number e = new Number(this.num2.value * a.num2.value);
+
+            return new Decimal(d, e);
+        }
+        public Decimal Divide(Decimal a)
+        {
+            Number d = new Number(this.num1.value * a.num2.value);
+            Number e = new Number(this.num2.value * a.num1.value);
+
+            return new Decimal(d, e);
+        }
+        public Decimal Plus(Decimal a)
+        {
+            Number d = new Number(this.num1.value * a.num2.value + this.num2.value * a.num1.value);
+            Number e = new Number(this.num2.value * a.num2.value);
+            
+            return new Decimal(d, e);
+        }
+        public Decimal Minus(Decimal a)
+        {
+            Number d = new Number(this.num1.value * a.num2.value - this.num2.value * a.num1.value);
+            Number e = new Number(this.num2.value * a.num2.value);
+
+            return new Decimal(d, e);
+        }
+
+    }
+}
+
